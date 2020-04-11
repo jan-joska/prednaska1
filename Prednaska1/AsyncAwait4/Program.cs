f8e71bffd1e240ce6e9a39677ac193d6a51f3861
Jan Joska <jan@joska.net>
2020-04-11 15:39:31 +0200

    add samples

---
+++ Prednaska1/AsyncAwait4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncAwait4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
